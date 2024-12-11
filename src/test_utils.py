bed6a87f20a7ad60caacde63265447be0aebf0f5
sj-jason-lee <jason.sj.lee@outlook.com>
2024-12-10 19:44:21 -0500

    add text to text node

--- src/test_utils.py
+++ src/test_utils.py
@@ -4,7 +4,8 @@ from utils import (
     extract_markdown_links,
     split_nodes_delimiter,
     split_nodes_image,
-    split_nodes_link
+    split_nodes_link,
+    text_to_textnodes
 )
 
 from textnode import TextNode, TextType
@@ -176,6 +177,24 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" with text that follows", TextType.TEXT),
             ],
             new_nodes,
+        )
+    
+    def test_text_to_textnode(self):
+        text = 'This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)'
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            text_to_textnodes(text)
         )
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -75,3 +75,20 @@ def split_nodes_image(old_nodes):
 		new_nodes.append(TextNode(original_text, TextType.TEXT))
 	return new_nodes
 
+def text_to_textnodes(text):
+	nodes = [TextNode(text, TextType.TEXT)]
+
+	nodes = split_nodes_image(nodes)
+	nodes = split_nodes_link(nodes)
+
+	delimiters = {
+		'**': TextType.BOLD,
+		'*': TextType.ITALIC,
+		'`': TextType.CODE
+	}
+
+	for delim in delimiters:
+		nodes = split_nodes_delimiter(nodes, delim, delimiters[delim])
+
+	return nodes
+
